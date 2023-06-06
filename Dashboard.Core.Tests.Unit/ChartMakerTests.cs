6c23f089e29738f7985930e42a53bd00d8c92338
James Coliz <jcoliz@outlook.com>
2023-06-05 21:29:24 -0700

    test(dash): Add first real test CreateMultiDeviceBarChart

---
+++ Dashboard.Core.Tests.Unit/ChartMakerTests.cs
@@ -0,0 +1,41 @@
+using BrewHub.Dashboard.Core.MockData;
+namespace Dashboard.Core.Tests.Unit;
+
+public class ChartMakerTests
+{
+    private readonly MockDataSource _datasource = new();
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task CreateMultiDeviceBarChart()
+    {
+        // Given: The latest (mock) telemetry from all devices
+        var raw = await _datasource.GetLatestDeviceTelemetryAllAsync();
+
+        // ...Fixed to a list, else the randoms will recalculate every time
+        var data = raw.ToList();
+
+        // And: Selecting a single component/field combination 
+        var component = data.Select(x => x.__Component).Distinct().OrderBy(x => x).Last();
+        var field = data.Select(x => x.__Field).Distinct().OrderBy(x => x).Last();
+        var label = $"{component}/{field}";
+
+        // When: Creating a chart from the returned data set focused on the selected label
+        var chart = BrewHub.Dashboard.Core.Charting.ChartMaker.CreateMultiDeviceBarChart(data, new[] { label });
+
+        // Then: The chart contains all the devices
+        var devices = data.Select(x => x.__Device).Distinct().OrderBy(x => x);
+        Assert.That(chart.Data.Labels,Is.EquivalentTo(devices));
+
+        // And: The label on the chart matches the label we asked for
+        Assert.That(chart.Data.Datasets.First().Label, Is.EqualTo(label));
+
+        // And: The values on the chart match the device values for that label
+        var values = data.Where(x => x.__Component == component && x.__Field == field).Select(x => Convert.ToInt32(x.__Value));
+        Assert.That(chart.Data.Datasets.First().Data,Is.EquivalentTo(values));
+    }
+}
