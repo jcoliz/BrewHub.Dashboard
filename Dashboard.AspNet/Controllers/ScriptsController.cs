e939ea866b452027cddb9695899fee673998c557
James Coliz <jcoliz@outlook.com>
2023-06-12 10:41:12 -0700

    feat(dash/api): Stub scripts controller

---
+++ Dashboard.AspNet/Controllers/ScriptsController.cs
@@ -0,0 +1,36 @@
+using BrewHub.Dashboard.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrewHub.Dashboard.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Produces("application/json")]
+public class ScriptsController : ControllerBase
+{
+    private readonly ILogger<ScriptsController> _logger;
+
+    public ScriptsController(ILogger<ScriptsController> logger)
+    {
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// Get list of all scripts
+    /// </summary>
+    /// <returns>Device names</returns>
+    [HttpGet]
+    [Route("")]
+    [ProducesResponseType(typeof(Script[]), StatusCodes.Status200OK)]
+    public ActionResult Scripts()
+    {
+        _logger.LogInformation("Scripts");
+
+        var result = new Script[]
+        {
+            new() { Name = "Cold Mash", Environment = "Production", Version = "1.0.10", Updated = DateTimeOffset.Now - TimeSpan.FromDays(30) }
+        };
+
+        return Ok(result);
+    }
+}
