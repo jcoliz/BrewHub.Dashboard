c822629a014a750fcdedb092520e721bf41ab40e
James Coliz <jcoliz@outlook.com>
2023-06-12 20:57:50 -0700

    refactor(dash): Remove needless old code

--- Dashboard.Core/Dtmi/KeyValueUnits.cs
+++
@@ -1,9 +0,0 @@
-namespace BrewHub.Dashboard.Core.Dtmi;
-
-public record KeyValueUnits
-{
-    public string Key { get; init; } = string.Empty;
-    public string Value { get; init; } = string.Empty;
-    public string? Units { get; init; } = null;
-    public bool Writable { get; init; } = false;
-}
